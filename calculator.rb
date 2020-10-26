1ae1bdd49903719b75d44678a7e445fd7006c456
searching-block-4535 <juancmezat@gmail.com>
2020-10-26 04:13:43 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,9 @@
 # Add your variables here
 
-first_number = rand(10)
-second_number = rand(10) + 1
-sum2 = first_number + second_number
-sum = sum2
+first_number = 3
+second_number = 4
+sum = first_number + second_number
+
 
 puts first_number
 puts second_number
